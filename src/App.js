9502437288a9782d83b823ddda4a7db120622117
Jayant Kaushik <jayantdps@gmail.com>
2022-12-15 18:53:41 +0530

    added experience section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-import Main from './components/Main';
+// import Main from './components/Main';
 import Hero from './components/Hero';
 import Card from './components/Card';
+import data from './data';
 
 function App() {
+
+
+  const cards = data.map(item => {
+    return (
+        <Card 
+            key={item.id}
+            img={item.coverImg}
+            rating={item.stats.rating}
+            reviewCount={item.stats.reviewCount}
+            location={item.location}
+            title={item.title}
+            price={item.price}
+        />
+    )
+})        
+
   return (
     <div className="App">
    
       <Navbar />
       {/* <Main /> */}
       <Hero />
-       <Card 
-                // img="katie-zaferes.png"
-                rating="5.0"
-                reviewCount={6}
-                country="USA"
-                title="Life Lessons with Katie Zaferes"
-                price={136}
-            />
+      <section  className="cards-list">
+      {cards}
+      </section>
+   
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,37 @@
 import React from 'react'
-import katie from '../images/katie-zaferes.png'
-import star from '../images/star.png'
+// import katie from '../images/katie-zaferes.png'
+// import star from '../images/star.png'
 
 export default function Card(props) {
   console.log(props)
   return (
-    <div className="card">
-    {/* <img src="../images/katie-zaferes.png" className="card--image" /> */}
-    <img src={katie} alt="card-img" className="card--image" />
-    {/* <img src={`../images/${props.img}`} className="card--image" /> */}
-    <div className="card--stats">
-        {/* <img src="../images/star.png" /> */}
-        <img src={star} alt="star" className="card--star" />
-        {/* <img src="../images/star.png" className="card--star" /> */}
-        <span>{props.rating}</span>
+//     <div className="card">
+//     {/* <img src="../images/katie-zaferes.png" className="card--image" /> */}
+//     {/* <img src={katie} alt="card-img" className="card--image" /> */}
+//     <img src={`../images/${props.img}`} className="card--image" />
+//     <div className="card--stats">
+//         <img src="../images/star.png" className="card--star"/>
+//         {/* <img src={star} alt="star" className="card--star" /> */}
+//         {/* <img src="../images/star.png" className="card--star" /> */}
+//         <span>{props.rating}</span>
+//     <span className="gray">({props.reviewCount}) • </span>
+//     <span className="gray">{props.country}</span>
+//             </div>
+//             <p>{props.title}</p>
+// <p><span className="bold">From ${props.price}</span> / person</p>
+//         </div>
+
+<div className="card">
+<img src={`../images/${props.img}`} className="card--image" />
+<div className="card--stats">
+    <img src="../images/star.png" className="card--star" />
+    <span>{props.rating}</span>
     <span className="gray">({props.reviewCount}) • </span>
-    <span className="gray">{props.country}</span>
-            </div>
-            <p>{props.title}</p>
-<p><span className="bold">From ${props.price}</span> / person</p>
-        </div>
-
-
+    <span className="gray">{props.location}</span>
+</div>
+<p className="card--title">{props.title}</p>
+<p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
+</div>
 
 
    
